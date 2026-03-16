cb0d5413a74112a0fc0afe23ee69ebdf082c42ba
muhammed sule dabban <muhammedsulai4@gmail.com>
2026-03-16 23:00:28 +0100

    progress

--- backend/ecommerce/src/main/java/com/example/ecommerce/config/auditConfig.java
+++ backend/ecommerce/src/main/java/com/example/ecommerce/config/auditConfig.java
@@ -1,14 +1,13 @@
 package com.example.ecommerce.config;
 
-import org.jspecify.annotations.Nullable;
+import java.util.Optional;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.domain.AuditorAware;
 import org.springframework.data.mongodb.config.EnableMongoAuditing;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
-
-import java.util.Optional;
 
 @Configuration
 @EnableMongoAuditing
